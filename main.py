d72f5e5670b58ed24981cb84c973cc6c951c0621
Luca Catalano <luca@MacBook-Pro-di-Luca.local>
2023-05-23 16:59:35 +0200

    utils.py updated

--- main.py
+++ main.py
@@ -114,10 +114,9 @@ class LightningModel(pl.LightningModule):
     def test_epoch_end(self, all_descriptors):
         return self.inference_epoch_end(all_descriptors, self.test_dataset, self.num_preds_to_save)
 
-    def inference_epoch_end(self, all_descriptors, inference_dataset, num_preds_to_save=0, ok = False):
-        if args.enable_gpm and ok:
+    def inference_epoch_end(self, all_descriptors, inference_dataset, num_preds_to_save=0):
+        if self.pbank is not None:
             self.pbank.update_index()
-        ok = True
         """all_descriptors contains database then queries descriptors"""
         all_descriptors = np.concatenate(all_descriptors)
         queries_descriptors = all_descriptors[inference_dataset.database_num : ]
